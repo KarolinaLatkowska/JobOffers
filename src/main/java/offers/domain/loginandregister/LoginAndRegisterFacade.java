e58270a7b5e10995c344ce56f234a9f821969ccf
Karolina Burzyńska <k.burzynska1@wp.pl>
2024-08-01 16:53:59 +0200

    added method: findByUsername and register

--- src/main/java/offers/domain/loginandregister/LoginAndRegisterFacade.java
+++ src/main/java/offers/domain/loginandregister/LoginAndRegisterFacade.java
@@ -1,4 +1,33 @@
 package offers.domain.loginandregister;
 
+import lombok.AllArgsConstructor;
+import offers.domain.loginandregister.dto.RegisterUserDto;
+import offers.domain.loginandregister.dto.RegistrationResultDto;
+import offers.domain.loginandregister.dto.UserDto;
+
+@AllArgsConstructor
 public class LoginAndRegisterFacade {
+
+    public static final String USER_NOT_FOUND = "User not found";
+
+    private final LoginRepository loginRepository;
+
+    public UserDto findByUsername(String username) {
+        return loginRepository.findByUsername(username)
+                .map(user -> new UserDto(user.id(), user.username(), user.password()))
+                .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+
+    }
+
+    public RegistrationResultDto register(RegisterUserDto registerUserDto) {
+        final User user = User.builder()
+                .username(registerUserDto.username())
+                .password(registerUserDto.password())
+                .build();
+        User savedUser = loginRepository.save(user);
+        return new RegistrationResultDto(savedUser.id(), true, savedUser.username());
+
+
+
+    }
 }
