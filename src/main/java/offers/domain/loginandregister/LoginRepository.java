61a8ac546f74016a64e3a6326018009f417abb95
Karolina Burzyńska <k.burzynska1@wp.pl>
2024-08-06 18:27:04 +0200

    created LoginRepository interface

---
+++ src/main/java/offers/domain/loginandregister/LoginRepository.java
@@ -0,0 +1,13 @@
+package offers.domain.loginandregister;
+
+
+import java.util.Optional;
+
+public interface LoginRepository {
+
+
+
+    Optional<User> findByUsername(String username);
+
+    User save(User user);
+}
